d6e138097a26ae2ddac3415882e9d9afe694158f
kawarimidoll <kawarimidoll+git@gmail.com>
2021-11-10 10:08:20 +0900

    test: add test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,23 @@
-console.log("there is no test now");
+import { assertEquals } from "./deps.ts";
+import { isDenoTest } from "./utils.ts";
+// import { isDenoTest, runProcess, watchChanges } from "./utils.ts";
+
+Deno.test("isDenoTest", () => {
+  assertEquals(isDenoTest("main.ts"), false);
+  assertEquals(isDenoTest("test.ts"), true);
+  assertEquals(isDenoTest("main.test.ts"), true);
+  assertEquals(isDenoTest("main_test.ts"), true);
+  assertEquals(isDenoTest("maintest.ts"), false);
+
+  assertEquals(isDenoTest("path/to/main.ts"), false);
+  assertEquals(isDenoTest("path/to/test.ts"), true);
+
+  assertEquals(isDenoTest("main.js"), false);
+  assertEquals(isDenoTest("main.test.js"), true);
+  assertEquals(isDenoTest("main.jsx"), false);
+  assertEquals(isDenoTest("main.test.jsx"), true);
+  assertEquals(isDenoTest("main.tsx"), false);
+  assertEquals(isDenoTest("main.test.tsx"), true);
+  assertEquals(isDenoTest("main.mjs"), false);
+  assertEquals(isDenoTest("main.test.mjs"), true);
+});
