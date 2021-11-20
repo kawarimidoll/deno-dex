de096d2d255a0ea10cf7d42fba9da5d8bf1973d0
kawarimidoll <kawarimidoll+git@gmail.com>
2021-11-20 16:42:57 +0900

    test: add test for parseCliArgs

--- test.ts
+++ test.ts
@@ -1,6 +1,101 @@
-import { assertEquals } from "./dev_deps.ts";
+import { assert, assertEquals } from "./dev_deps.ts";
+import { parseCliArgs } from "./main.ts";
 import { isDenoTest } from "./utils.ts";
 // import { isDenoTest, runProcess, watchChanges } from "./utils.ts";
+
+Deno.test("[parseCliArgs] pattern 1", () => {
+  const {
+    args,
+    clear,
+    debug,
+    help,
+    quiet,
+    version,
+    watch,
+    runOptions,
+  } = parseCliArgs([
+    "--allow-all",
+    "-q",
+    "main.ts",
+    "--script-flag",
+    "script-arg",
+  ]);
+
+  assertEquals(args, ["main.ts", "--script-flag", "script-arg"]);
+  assert([clear, debug, help, version].every((flag) => flag === false));
+  assert(quiet);
+  assertEquals(watch, []);
+  assertEquals(runOptions, [
+    "--allow-all",
+    "--no-check",
+    "--unstable",
+    "--watch",
+    "--quiet",
+  ]);
+});
+
+Deno.test("[parseCliArgs] pattern 2", () => {
+  const {
+    args,
+    clear,
+    debug,
+    help,
+    quiet,
+    version,
+    watch,
+    runOptions,
+  } = parseCliArgs([
+    "--watch=a.txt,b.txt",
+    "--no-check",
+    "--unstable",
+    "--compat",
+    "--clear",
+    "main.ts",
+  ]);
+
+  assertEquals(args, ["main.ts"]);
+  assert([debug, help, quiet, version].every((flag) => flag === false));
+  assert(clear);
+  assertEquals(watch, ["a.txt", "b.txt"]);
+  assertEquals(runOptions, [
+    "--allow-all",
+    "--no-check",
+    "--unstable",
+    "--watch",
+    "--compat",
+  ]);
+});
+
+Deno.test("[parseCliArgs] pattern 3", () => {
+  const {
+    args,
+    clear,
+    debug,
+    help,
+    quiet,
+    version,
+    watch,
+    runOptions,
+  } = parseCliArgs([
+    "-Aqfc",
+    "deno.json",
+    "main.ts",
+  ]);
+
+  assertEquals(args, ["main.ts"]);
+  assert([clear, debug, help, version].every((flag) => flag === false));
+  assert(quiet);
+  assertEquals(watch, []);
+  assertEquals(runOptions, [
+    "--allow-all",
+    "--no-check",
+    "--unstable",
+    "--watch",
+    "-f",
+    "-c=deno.json",
+    "--quiet",
+  ]);
+});
 
 Deno.test("isDenoTest", () => {
   assertEquals(isDenoTest("main.ts"), false);
