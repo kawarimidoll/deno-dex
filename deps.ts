aa1699a306bef2af67a00a495157248b45188466
GitHub <noreply@github.com>
2023-03-11 01:06:33 +0000

    chore(deps): update deno dependencies

--- deps.ts
+++ deps.ts
@@ -3,14 +3,14 @@ export {
   brightBlue,
   green,
   red,
-} from "https://deno.land/std@0.177.0/fmt/colors.ts";
+} from "https://deno.land/std@0.179.0/fmt/colors.ts";
 export {
   basename,
   dirname,
   join,
   relative,
   resolve,
-} from "https://deno.land/std@0.177.0/path/mod.ts";
-export { parse } from "https://deno.land/std@0.177.0/flags/mod.ts";
-export { ensureDir } from "https://deno.land/std@0.177.0/fs/ensure_dir.ts";
+} from "https://deno.land/std@0.179.0/path/mod.ts";
+export { parse } from "https://deno.land/std@0.179.0/flags/mod.ts";
+export { ensureDir } from "https://deno.land/std@0.179.0/fs/ensure_dir.ts";
 export { getDenoDir } from "https://gist.githubusercontent.com/kawarimidoll/92179f60dfc67de3b0a52c5eb25ad333/raw/b193e088e47ee033a7d685ca6e9f45793bd26844/get_deno_dir.ts";
