8c888269dfcb7a2dca5d8dcca3165c31dbe3f33c
GitHub <noreply@github.com>
2024-04-26 01:00:02 +0000

    chore(deps): update deno dependencies

--- dev_deps.ts
+++ dev_deps.ts
@@ -2,5 +2,5 @@ export {
   assert,
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.222.1/testing/asserts.ts";
-export { serve } from "https://deno.land/std@0.222.1/http/server.ts";
+} from "https://deno.land/std@0.224.0/testing/asserts.ts";
+export { serve } from "https://deno.land/std@0.224.0/http/server.ts";
